78acd9eb9b86fce36e810816d4818f44cdba3a6d
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-09 16:57:18 -0300

    feat: add more edges and move to a cronjob

--- src/routes/measure/+server.ts
+++ src/routes/measure/+server.ts
@@ -1,76 +1,69 @@
 import type { RequestHandler } from "../$types";
 
-async function measureTimes(edge, stub, platform, request, fetchFn) {
-    let key = `edge:${edge}:${new Date().setMinutes(0, 0, 0)}`
-    let result = await platform?.env.TIMES.get(key)
+let edges = [
+    {
+        edge: 'cloudflare-worker',
+    },
+    {
+        edge: 'cloudflare-worker-tinygo',
+    },
+    {
+        edge: 'vercel-go',
+    },
+    {
+        edge: 'vercel-nodejs',
+    },
+    {
+        edge: 'vercel-rust',
+    },
+    {
+        edge: 'vercel-bun',
+    },
+    {
+        edge: 'deno-deploy',
+    },
+    {
+        edge: 'deno-deploy-go',
+    },
+    {
+        edge: 'netlify',
+    },
+    {
+        edge: 'netlify-go',
+    },
+    {
+        edge: 'fastly-tinygo',
+    },
+    {
+        edge: 'fastly-js',
+    },
+    {
+        edge: 'fastly-rust',
+    },
+]
 
-    if (result) {
-        let saved = JSON.parse(result)
-        for (let i = 0; i < saved.length; i++) {
-            stub.pushData(JSON.stringify(saved[i]))
-        }
-
-        return
-    }
-
-    let srcDc = request.cf?.colo || 'Unknown'
-    let results = []
+async function measureTimes(stub, platform) {
     let proms = []
-    let measures = { edge, time: Date.now(), error: undefined, datacenter: `${srcDc} -> ` }
-    proms.push(stub.pushData(JSON.stringify(measures)))
-    results.push(structuredClone(measures))
+    for (let i = 0; i < edges.length; i++) {
+        let key = `edge:${edges[i]}:${new Date().setMinutes(0, 0, 0)}`
+        let result = await platform?.env.TIMES.get(key)
 
-    let rs = await fetchFn()
-
-    measures.datacenter += rs.headers.get('x-datacenter') || "Unknown"
-
-    if (!rs.ok) {
-        measures.time = Date.now()
-        measures.error = rs.status
-        proms.push(stub.pushData(JSON.stringify(measures)))
-        results.push(structuredClone(measures))
-
-        console.error('error fetching api', rs.status, rs.url)
-        await Promise.allSettled(proms)
-    } else {
-        let body = await rs.json()
-        measures.time = body.time
-        proms.push(stub.pushData(JSON.stringify(measures)))
-        results.push(structuredClone(measures))
-
-        measures.time = Date.now()
-        proms.push(stub.pushData(JSON.stringify(measures)))
-        results.push(structuredClone(measures))
-
-        proms.push(platform.env.TIMES.put(key, JSON.stringify(results)))
-        await Promise.allSettled(proms)
+        if (result) {
+            let saved = JSON.parse(result)
+            for (let i = 0; i < saved.length; i++) {
+                proms.push(stub.pushData(JSON.stringify(saved[i])))
+            }
+        }
     }
+
+    await Promise.allSettled(proms)
 }
 
-export const GET: RequestHandler = async ({ platform, request }) => {
+export const GET: RequestHandler = async ({ platform }) => {
     let stub = platform?.env.DURABLE_SSE.getByName("cold-starts")
     let stream = await stub.createStream()
 
-    let cfWorkerFn = () => platform.env.WORKER.fetch("http://dummy.url", {
-        method: "post",
-        Headers: {
-            'content-type': 'application/json'
-        },
-    })
-
-    let cfWorkerGoFn = () => platform.env.WORKER_GO.fetch("http://dummy.url", {
-        method: "post",
-        Headers: {
-            'content-type': 'application/json'
-        },
-    })
-
-    platform?.ctx.waitUntil((async () => {
-        await Promise.allSettled([
-            measureTimes('cloudflare-worker', stub, platform, request, cfWorkerFn),
-            measureTimes('cloudflare-worker-go', stub, platform, request, cfWorkerGoFn)
-        ])
-    })())
+    platform?.ctx.waitUntil(measureTimes(stub, platform))
 
     return new Response(stream, {
         headers: {
